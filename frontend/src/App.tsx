64f4746d4776a453283ba47d4da8ce4edc0dd323
Eugene Briones <eugenebriones7135@gmail.com>
2026-05-09 15:25:20 +1000

    initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SampleComponent from './components/SampleComponent/SampleComponent'
+
+function App() {
+    return (
+        <>
+            Hi this is frontend
+            <SampleComponent />
+        </>
+    )
+}
+
+export default App
---
+++ frontend/src/components/SampleComponent/SampleComponent.tsx
@@ -0,0 +1,13 @@
+import styles from './SampleComponent.module.css'
+
+const SampleComponent = () => {
+    return (
+        <>
+            <div className={styles.sampleStyle}>
+                Hi I am the sample component
+            </div>
+        </>
+    )
+}
+
+export default SampleComponent;
