2534fabed0339dc1a693be5ae5cbf1309e15c187
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-05-14 14:29:56 +1000

    Adding routes to App.tsx frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css'
-import LandingPage from './components/LandingPage/LandingPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import './App.css';
+
 function App() {
-    return (
-        <>
-            
-            <LandingPage />
-        </>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add other routes here */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
