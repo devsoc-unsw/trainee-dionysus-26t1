37736e8b12af0a3559fa036abc2501622656a566
Eugene Briones <eugenebriones7135@gmail.com>
2026-05-16 11:52:55 +1000

    Fixed up backend removed server.ts to have all server-side logic lie in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,12 +11,32 @@ app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
-	res.json({ message: "Backend is running!" });
+    res.json({ message: "Backend is running!" });
 });
 
 // TODO: Add routes to add users and sleep event things
 
+app.post('/api/login/signup', (req, res) => {
+    // TODO: create a user in DB (once DB is set up)
+    console.log(req.body.username)
+    console.log(req.body.email)
+
+    const token = jwt.sign(
+        { username: req.body.username, email: req.body.email },
+        process.env.JWT_SECRET!,
+        { expiresIn: '7d' }
+    );
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,       // HTTPS only
+        sameSite: 'strict', // CSRF protection
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    })
+
+    res.sendStatus(200);
+});
 
 app.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
