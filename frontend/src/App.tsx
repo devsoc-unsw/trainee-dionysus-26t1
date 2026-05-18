aae1a8f88394bc4bf143f1f701b7d0907124a725
Evan Luong <evan.luong@student.unsw.edu.au>
2026-05-18 13:46:02 +1000

    Created Leaderboard files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage/LandingPage'
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute'
 import SampleComponent from './components/SampleComponent/SampleComponent'
 import SignupPage from './pages/SignupPage/SignupPage'
+import LeaderboardPage from './pages/LeaderboardPage/LeaderboardPage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/signup' element={<SignupPage />} />
+                <Route path='/leaderboard' element={<LeaderboardPage />} />
 
                 {/* Routes that need auth */}
                 <Route element={<ProtectedRoute />}>
---
+++ frontend/src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -0,0 +1,15 @@
+// import styles from './Leaderboard.module.css'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const LeaderboardPage = () => {
+    
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default LeaderboardPage;
